be48c52d73c820577ed2c994e183d3f5cc892089
Lisa Schoofs <lisaschoofs@gmail.com>
2017-03-19 16:09:28 -0500

    sets up POST that sends object to server

--- server/app.js
+++ server/app.js
@@ -4,19 +4,25 @@ var path = require('path');
 var port = 5000;
 
 app.use(express.static("server/public"));
-//
+
+//enables bodyParser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+//post for object from client
+app.post("/calculator", function(req, res) {
+  //do some sort of magical mathematical calculations here!
+  console.log(req.body);
+  res.sendStatus(200);
+});
 
 
-
-
-//
+//catch-all. if there isn't a match, send to index.html
 app.get("/", function(req, res) {
   res.sendFile(path.resolve("server/public/views/index.html"));
 });
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-
+//port listening
 app.listen(port, function() {
   console.log("listening on port: ", port);
 });
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,24 +4,47 @@ $(document).ready(function(){
 
 function eventListeners() {
 
-  $("#addButton").on("click", function() {
-    console.log("addButton clicked");
-  });
 
-  $("#subtractButton").on("click", function() {
-    console.log("subtractButton clicked");
-  });
+  $("#buttons").on("click", "button", function() {
+    console.log("a button has been clicked");
 
-  $("#multiplyButton").on("click", function() {
-    console.log("multiplyButton clicked");
-  });
+    //gets user input, assembles into object
+    var objectToSend = {
+      x: $('#valOne').val(),
+      y: $('#valTwo').val(),
+      type: ($(this).attr('id'))
+    };
 
-  $("#divideButton").on("click", function() {
-    console.log("divideButton clicked");
-  });
+    $.ajax({
+      type: 'POST',
+      url: '/calculator',
+      data: objectToSend,
+      success: function( response ) {
+        console.log('back from calculator with: ', response);
+        // will need to later append to DOM...
+      } //ends success
+    }); //ends ajax post
+  });//ends buttons listener
 
-  $("#clearButton").on("click", function() {
-    console.log("clearButton clicked");
-  });
 
 } //end eventListeners
+
+// $("#addButton").on("click", function() {
+//   console.log("addButton clicked");
+// });
+//
+// $("#subtractButton").on("click", function() {
+//   console.log("subtractButton clicked");
+// });
+//
+// $("#multiplyButton").on("click", function() {
+//   console.log("multiplyButton clicked");
+// });
+//
+// $("#divideButton").on("click", function() {
+//   console.log("divideButton clicked");
+// });
+//
+// $("#clearButton").on("click", function() {
+//   console.log("clearButton clicked");
+// });
