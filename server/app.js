6f8b167a244ff8b38e7f9d37d97479ae9eb852c2
Lisa Schoofs <lisaschoofs@gmail.com>
2017-03-18 20:58:44 -0500

    adds body-parser in app.js

--- server/app.js
+++ server/app.js
@@ -14,6 +14,9 @@ app.get("/", function(req, res) {
   res.sendFile(path.resolve("server/public/views/index.html"));
 });
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.listen(port, function() {
   console.log("listening on port: ", port);
 });
